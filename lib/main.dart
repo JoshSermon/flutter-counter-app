34b136f556dabbbb06eb8525e5caf60f94372729
Joshua Sermon <JoshuaSermon1@gmail.com>
2025-11-19 23:51:52 -0600

    building flutter app - adding a button with functionality part 3

--- lib/main.dart
+++ lib/main.dart
@@ -41,11 +41,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
-          Text('A random AWESOME idea:'), Text(appState.current.asLowerCase),
+          Text('A random AWESOME idea:'), 
+          BigCard(pair: pair),
             ElevatedButton(
               onPressed: () {
                 appState.getNext();
@@ -65,10 +67,15 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     final theme = Theme.of(context); 
+     final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,);
+
     return Card(
+      color: theme.colorScheme.primary, 
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
